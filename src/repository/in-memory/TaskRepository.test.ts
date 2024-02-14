48b4e0c53df01e811399ca9b3445d62da8ce96fd
lpkyrius <lpkyrius@hotmail.com>
2024-02-14 14:29:01 +0000

    complementing covered tests

--- src/repository/in-memory/TaskRepository.test.ts
+++ src/repository/in-memory/TaskRepository.test.ts
@@ -3,12 +3,17 @@ import { TasksRepositoryInMemory } from './tasks/TaskRepositoryInMemory';
 import { TaskRepositoryInPostgres } from '../postgres/tasks/TaskRepositoryInPostgres';
 import { ITaskRepository } from '../ITaskRepository';
 
+// Mock console.log and console.error globally for the entire test suite
+// So we keep a clear console when tests should return error 
+global.console.log = jest.fn();
+global.console.error = jest.fn();
+
 describe('#taskService', () => {
 
   // to enable/disable one specific test (in memory or postgres) 
   // just comment the correspondent line withing the repositories object below
   const repositories: Record<string, string> = { 
-    // inmemory: 'InMemory', 
+    inmemory: 'InMemory', 
     database: 'Postgres' 
   };
 
@@ -25,7 +30,7 @@ describe('#taskService', () => {
           }
         });
 
-        describe.skip('#Find Task by Id', () => {
+        describe('#Find Task by Id', () => {
           it(`should throw an error if the task ID is not found - ${ repositories[property] }`, async () => { 
               try {
                 await tasksRepository.findTaskById('this.id.should.not.exist');
@@ -35,7 +40,7 @@ describe('#taskService', () => {
             });
         });
 
-        describe.skip('#Delete Tasks', () => {
+        describe('#Delete Tasks', () => {
           it(`should return false when deleting a non-existing task on RepositoryInMemory - ${ repositories[property] }`, async () => {
             const testDel = async function () {
               return await tasksRepository.delete('this.id.should.not.exist');
